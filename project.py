1a05b4aa564c860bc10edd071c0de615562caadd
yokesuntanon <apijitrawee2003@gmail.com>
2023-10-24 10:21:23 +0700

    "added TaxWindow Class"

--- project.py
+++ project.py
@@ -1,9 +1,16 @@
 import tkinter as tk
 
+
+class TaxWindow:
+    def __init__(self) -> None:
+        self.window = tk.Tk()
+        self.window.title("Tax")
+        self.window.mainloop()
+
 window = tk.Tk()
 window.title("App")
 
-button_1 = tk.Button(master=window, text="Tax")
+button_1 = tk.Button(master=window, text="Tax", command=TaxWindow)
 button_2 = tk.Button(master=window, text="Comparison")
 
 button_1.pack()
